87c31bd120394a8446651cadec46923f9bf831c9
Abhishek Raj <abhishekraj995555@gmail.com>
2025-10-15 22:59:00 +0530

    Styling Header part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <div className="min-h-screen bg-white">
+      <Header/>
+    </div>
+    </>
   )
 }
 
