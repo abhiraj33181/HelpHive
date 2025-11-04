ec26c96ba78e37f89cca796f155d2a4fb09f5649
Abhishek Raj <abhishekraj995555@gmail.com>
2025-11-04 18:16:08 +0530

    adding cors origin

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : ['http://localhost:5173', 'https://helphive-liard.vercel.app'],
     credentials: true
 }))
 app.use(express.json())
